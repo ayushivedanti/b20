76e8c4853f6f438a9a1740242578175096c32b57
Asus <Asus@DESKTOP-DK89G76>
2021-04-06 09:01:28 -0700

    running main method m1...

---
+++ src/methods/Main_method.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class Main_method {
+	public static void main(String[] args) {
+		
+		System.out.println("main method started");
+		
+		m1();
+		System.out.println("main method ended");
+	}
+		public static void m1() {
+			System.out.println("running method m1");
+			
+			
+		}
+	}
+
+
